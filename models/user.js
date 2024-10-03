9e5f91891f74994461808aaac3bb5b91e15412e0
Edgar Lopez <edgarlopez755@gmail.com>
2024-10-02 21:04:46 -0700

    Add to the Models/user

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const matchSchema = new mongoose.Schema({
+    homeTeam: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team'
+    },
+    awayTeam: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Team'
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    homeScore: {
+        type: Number,
+        default: 0
+    },
+    awayScore: {
+        type: Number,
+        default: 0
+    }
+})
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const Match = mongoose.model('Match', matchSchema)
+const User = mongoose.model('User', userSchema)
+
+module.exports = Match
+module.exports = User
+
+        
